aee85ed6eb88e39d2df860225f5886a091107043
jxmked <jovandeguia@gmail.com>
2025-03-12 12:26:09 +0800

    update

--- generate_build_flags.py
+++ generate_build_flags.py
@@ -1,6 +1,9 @@
 import os
 
 build_flags = [
+    "// DO NOT MODIFY MANUALLY!!!",
+    "#ifndef ___DOTENV_h",
+    "#define ___DOTENV_h",
     "#define stringify(s) _stringifyDo(s)",
     "#define _stringifyDo(s) #s"
 ]
@@ -13,6 +16,8 @@ with open("./.env", "r") as file:
         value = line.split("=")[1]
 
         build_flags.append(f"#define {key} {value}")
+
+build_flags.append("#endif")
 
 with open("include/DOTENV.h", "w+") as file:
     file.write("\n".join(build_flags))
