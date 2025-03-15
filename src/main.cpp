0d6927d6942b50fc3f3a1d8dda1162f15b6888ae
jxmked <jovandeguia@gmail.com>
2025-03-15 15:35:20 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <driver/gpio.h>
 struct __emit_num_values {
   uint8_t leds[7];
   uint8_t led_count;
@@ -10,30 +10,14 @@ struct __emit_obj {
   uint8_t b;
 };
 
-uint8_t pins[5] = {5, 6, 7, 8, 9};
-// uint8_t OBJ[3][7][2] = {
-//     {{3, 4}, {3, 5}},
-//     {{4, 2}, {4, 5}, {5, 4}, {4, 3}, {5, 3}, {5, 2}, {3, 2}},
-
-//     {{6, 3}, {5, 6}, {6, 5}, {4, 6}, {6, 4}, {3, 6}, {6, 2}}};
-
-// 3 5 6 9 10
-// 5 6 7 8 9
-
 // HIGH-LOW
-uint8_t OBJ[3][7][2] = {
-    {{6, 7}, {6, 8}},
-    {{7, 5}, {7, 8}, {8, 7}, {7, 6}, {8, 6}, {8, 5}, {6, 5}},
+__emit_obj OBJ[3][7] = {
+    // Need to fill some so with empty (0, 1) so we can normally use anything.
+    {{0, 1}, {0, 1}, {1, 2}, {1, 3}, {0, 1}, {0, 1}, {0, 1}},
 
-    {{9, 6}, {8, 9}, {9, 8}, {7, 9}, {9, 7}, {6, 9}, {9, 5}}};
-
-// 75 42 21
-// 78 45 22
-// 87 54 23
-// 76 43 24
-// 86 53 25
-// 85 52 26
-// 65 32 27
+    // This (2, 3) are replace by (0, 1) since (2, 3) are damaged
+    {{2, 0}, {0, 1}, {3, 2}, {2, 1}, {3, 1}, {3, 0}, {1, 0}},
+    {{4, 1}, {3, 4}, {4, 3}, {2, 4}, {4, 2}, {1, 4}, {4, 0}}};
 
 __emit_num_values num_combo[10] = {
     {{0, 1, 2, 3, 4, 5}, 6}, {{2, 3}, 2},       {{1, 2, 4, 5, 6}, 5},
@@ -41,90 +25,113 @@ __emit_num_values num_combo[10] = {
     {{0, 1, 3, 4, 5, 6}, 6}, {{1, 2, 3}, 3},    {{0, 1, 2, 3, 4, 5, 6}, 7},
     {{0, 1, 2, 3, 4, 6}, 6}};
 
-void combo(uint8_t a, uint8_t b) {
-  for (int i = 0; i < 5; i++) {
-    uint8_t p = pins[i];
+int sx = 5;
+int sy = 10;
 
-    if (p == a || p == b) {
-      pinMode(p, OUTPUT);
-    } else {
-      pinMode(p, INPUT);
-    }
-  }
-
-  delay(100);
-
-  digitalWrite(a, LOW);
-  digitalWrite(b, HIGH);
-}
+uint8_t pins[5] = {A0, A1, A2, A3, A4};
+// gpio_num_t pins[5] = {GPIO_NUM_10, GPIO_NUM_9, GPIO_NUM_8, GPIO_NUM_7,
+//                       GPIO_NUM_6};
 
 void reset_leds() {
-  for (int i = 0; i < 5; i++) {
-    pinMode(pins[i], INPUT);
-  }
+  for (int i = 0; i < 5; i++) pinMode(pins[i], INPUT_PULLDOWN);
+  // for (int i = 0; i < 5; i++) gpio_reset_pin(pins[i]);
+
+  // delayMicroseconds(100);
+}
+
+void combo(uint8_t a, uint8_t b) {
+  uint8_t ta = pins[a];
+  uint8_t tb = pins[b];
+  // gpio_num_t ta = pins[a];
+  // gpio_num_t tb = pins[b];
+
+  // gpio_set_direction(ta, GPIO_MODE_OUTPUT);
+  // gpio_set_direction(tb, GPIO_MODE_OUTPUT);
+
+  for (int asd = 0; asd < 2; asd++)
+    for (int i = 0; i < 5; i++) {
+      pinMode(pins[i], INPUT_PULLUP);
+    }
+
+  digitalWrite(ta, LOW);
+  digitalWrite(tb, LOW);
+  digitalWrite(ta, LOW);
+  digitalWrite(tb, LOW);
+
+  pinMode(ta, OUTPUT);
+  pinMode(tb, OUTPUT);
+
+  digitalWrite(ta, LOW);
+  digitalWrite(tb, LOW);
+
+  digitalWrite(tb, HIGH);
+
+  // Need everything to set to low so
+  // We not disturb other leds
+  // gpio_set_level(tb, 0);
+  // gpio_set_level(ta, 0);
+  // delayMicroseconds(100);
+  // gpio_set_level(tb, 1);
 }
 
 void emit_led(uint8_t seg, uint8_t index) {
+  // reset_leds();
+
   seg = seg % 3;
 
-  if (seg == 0) {
-    index = index % 2;
-  }
+  index %= 7;
 
-  uint8_t x = OBJ[seg][index][0];
-  uint8_t y = OBJ[seg][index][1];
+  __emit_obj res = OBJ[seg][index];
 
-  reset_leds();
-  combo(x, y);
-}
-
-bool within_array(uint8_t arr[7], uint8_t target) {
-  for (int i = 0; i < 7; i++) {
-    if (arr[i] == target) return true;
-  }
-
-  return false;
+  combo(res.a, res.b);
 }
 
 void emit_num(uint8_t seg, uint8_t num) {
   if (seg == 0) {
-    if (!num == 1) return;
+    if (num != 1) return;
   }
 
   __emit_num_values res_num = num_combo[num];
 
-  // int ah = 0;
-  // int al = 0;
-  // int ahb = 0;
-  // int alb = 0;
-  // uint8_t all_high[7];
-  // uint8_t all_low[7];
-
-  // uint8_t all_high_b[7];
-  // uint8_t all_low_b[7];
-
   for (int i = 0; i < res_num.led_count; i++) {
-    uint8_t x = OBJ[seg][res_num.leds[i]][0];
-    uint8_t y = OBJ[seg][res_num.leds[i]][1];
+    uint8_t n = res_num.leds[i];
 
-    // if (!within_array(all_high, x)) {
-    //   all_high[ah] = x;
-    //   ah++;
-    // }
+    emit_led(seg, n);
 
-    // if (!within_array(all_low, y)) {
-    //   all_low[al] = y;
-    //   al++;
-    // }
+    delay(0);
+  }
+}
 
-    // reset_leds();
-    // delay(5);
-    combo(x, y);
-    delay(4);
+/**
+ * Being used to map leds
+ *
+ */
+void handle_serial() {
+  if (Serial.available() > 0) {
+    String inputString = Serial.readStringUntil('\n');
+    inputString.trim();
+
+    if (inputString.length() == 2 &&
+        (inputString[0] >= '0' && inputString[0] <= '9') &&
+        (inputString[1] >= '0' && inputString[1] <= '9')) {
+      int tensDigit = inputString[0] - '0';  // Convert char to int
+      int onesDigit = inputString[1] - '0';  // Convert char to int
+
+      Serial.print("Tens digit: ");
+      Serial.println(tensDigit);
+      Serial.print("Ones digit: ");
+      Serial.println(onesDigit);
+
+      sx = tensDigit;
+      sy = onesDigit;
+    } else {
+      Serial.println("Invalid input. Please enter a two-digit number.");
+    }
   }
 }
 
 void setup() {
+  // setCpuFrequencyMhz(10);
   Serial.begin(9600);
 
   while (!Serial) {
@@ -133,6 +140,10 @@ void setup() {
 }
 
 void loop() {
-  emit_num(2, 8);
-  delay(10);
-};
+  uint8_t count = (millis() / 1000) % 100;
+  // handle_serial();
+  // combo(sx, sy);
+  emit_num(0, 1);
+  emit_num(1, (count / 10) % 10);
+  emit_num(2, count % 10);
+}
